9c8e4f86a0562e88f9ac6c631f54d5d88753855e
mattia richetto <mrichetto@opentable.com>
2016-08-29 13:05:51 +0100

    from RegExp to eqeqeq

--- middleware.js
+++ middleware.js
@@ -4,9 +4,7 @@ import App from './app/app'
 
 module.exports = (options) =>
   (req, res, next) => {
-    if (req.url.match(new RegExp(options.middlewarePath, 'i'))) {
-      // TODO: pass queryString Params as props to the rendered component
-      // TODO: add sfx bundle
+    if (req.url === options.middlewarePath) {
       const html =
         `
         <!DOCTYPE html>
