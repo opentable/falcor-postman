e043a07bb2965418c275aeeeb5f42b29ef83863e
mattia richetto <mrichetto@opentable.com>
2016-10-21 22:57:05 +0100

    use RegExp

--- middleware/index.js
+++ middleware/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const path = require('path');
 
 module.exports = ({ middlewarePath = '/falcor-postman', app }) => {
-  app.use(express.static(path.join(__dirname, '/../dist/')));
+  app.use(express.static(path.join(__dirname, '/../dist/'), { redirect: false }));
 
   return (req, res, next) => {
-    if (req.url === middlewarePath) {
+    const regexp = new RegExp(`${middlewarePath}(/.*)?$`);
+    if (regexp.test(req.url)) {
       res.status(200);
       res.sendFile(path.join(__dirname, '/../dist/falcor-postman.html'));
       return;
